ed75b18684cf9f4b291d17cc4adf002c90501582
withat-sh <withat@salary-hero.com>
2026-04-29 10:51:49 +0700

    fix: add test steps, add env, add sample entpoints

--- api/tests/signup/benz-signup-employee-id.test.ts
+++ api/tests/signup/benz-signup-employee-id.test.ts
@@ -1,243 +1,278 @@
 import { test, expect } from '@playwright/test'
+import dotenv from 'dotenv'
+
+dotenv.config()
+const firebaseAPIKey = process.env.FIREBASE_API_KEY
+const baseUrl = process.env.API_BASE_URL
+const appVersion = process.env.X_APP_VERSION || ''
+
+const signupEndpoints = {
+  employeeId: {
+    signup: '/api/v2/public/account/signup/employee-id',
+    addPhoneRequest: '/api/v2/public/account/signup/employee-id/add-phone?verification_method=otp&action=request',
+    addPhoneVerify: '/api/v2/public/account/signup/employee-id/add-phone?verification_method=otp&action=verify',
+  },
+  verifyPin: '/api/v1/user/account/profile/pincode/create',
+}
+
+const firebaseEndpoints = {
+  signup: 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=',
+  secureToken: 'https://securetoken.googleapis.com/v1/token?key=',
+}
+
+const userEndpoints = {
+  profile: '/api/v1/user/account/profile',
+  logout: '/api/v1/user/account/profile/logout',
+}
 
 const endpoints = {
   signupEmployeeId:
-    'https://apiv2-staging.salary-hero.com/api/v3/public/account/signup/employee-id',
+    `${baseUrl}${signupEndpoints.employeeId.signup}`,
   verifySignupEmployeeIdAddPhone:
-    'https://apiv2-staging.salary-hero.com/api/v2/public/account/signup/employee-id/add-phone?verification_method=otp&action=request',
+    `${baseUrl}${signupEndpoints.employeeId.addPhoneRequest}`,
   verifySignupEmployeeIdAddPhoneVerify:
-    'https://apiv2-staging.salary-hero.com/api/v2/public/account/signup/employee-id/add-phone?verification_method=otp&action=verify',
+    `${baseUrl}${signupEndpoints.employeeId.addPhoneVerify}`,
   signupFirebase:
-    'https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyBeV5kfq740yW7LLhz_W2fPalGmd34gSXk',
+    `${firebaseEndpoints.signup}${firebaseAPIKey}`,
   secureFirebaseToken:
-    'https://securetoken.googleapis.com/v1/token?key=AIzaSyBeV5kfq740yW7LLhz_W2fPalGmd34gSXk',
+    `${firebaseEndpoints.secureToken}${firebaseAPIKey}`,
   verifyPin:
-    'https://apiv2-staging.salary-hero.com/api/v1/user/account/profile/pincode/create',
+    `${baseUrl}${signupEndpoints.verifyPin}`,
   userProfile:
-    'https://apiv2-staging.salary-hero.com/api/v1/user/account/profile',
+    `${baseUrl}${userEndpoints.profile}`,
   userLogout:
-    'https://apiv2-staging.salary-hero.com/api/v1/user/account/profile/logout',
+    `${baseUrl}${userEndpoints.logout}`,
 }
 
 const employeeData = {
   employee_id: 'benzpassport01',
   passport_no: 'A260422',
-  company_id: '1288',
+  company_id: 1288,
   phone: '0881001995',
   otp: '199119',
   firstName: 'Benz',
   pinCode: '000000',
 }
 
-const appVersion = '5.4.1'
+
 
 test.describe('Signup by Employee ID', () => {
   test('signup with valid employee id and passport number', async ({
     request,
   }) => {
-    // 1. Input Employee ID and Passport Number
-    const signupEmployeeIdResponse = await request.post(
-      endpoints.signupEmployeeId,
-      {
-        data: {
-          employee_id: employeeData.employee_id,
-          identity: employeeData.passport_no,
-          company_id: employeeData.company_id,
-        },
-      }
-    )
+    let authChallenge: string
+    let refCode: string
+    let firebaseToken: string
+    let firebaseRefreshToken: string
+    let idTokenBeforeVerifyPin: string
+    let idTokenAfterVerifyPin: string
 
-    const signupEmployeeIdResponseBody = await signupEmployeeIdResponse.json()
-    // console.log('signupEmployeeIdResponseBody: ', signupEmployeeIdResponseBody)
-
-    expect(signupEmployeeIdResponseBody.is_signup).toBe(false)
-    expect(
-      typeof signupEmployeeIdResponseBody.verification_info.auth_challenge
-    ).toBe('string')
-
-    const authChallenge =
-      signupEmployeeIdResponseBody.verification_info.auth_challenge
-
-    // 2. Input Phone Number
-    const verifyAddPhoneResponse = await request.post(
-      endpoints.verifySignupEmployeeIdAddPhone,
-      {
-        data: {
-          phone: employeeData.phone,
-          auth_challenge: authChallenge,
-        },
-      }
-    )
-
-    const verifyAddPhoneResponseBody = await verifyAddPhoneResponse.json()
-    // console.log('verifyAddPhoneResponseBody: ', verifyAddPhoneResponseBody)
-
-    expect(typeof verifyAddPhoneResponseBody.verification.ref_code).toBe(
-      'string'
-    )
-
-    const refCode = verifyAddPhoneResponseBody.verification.ref_code
-
-    // 3. Verify Phone Number - Employee ID Signup
-    const verifyEmployeeIdSignupResponse = await request.post(
-      endpoints.verifySignupEmployeeIdAddPhoneVerify,
-      {
-        data: {
-          phone: employeeData.phone,
-          auth_challenge: authChallenge,
-          verification: {
-            ref_code: refCode,
-            code: employeeData.otp,
+    await test.step('Step 1: Input Employee ID and Passport Number', async () => {
+      const signupEmployeeIdResponse = await request.post(
+        endpoints.signupEmployeeId,
+        {
+          data: {
+            employee_id: employeeData.employee_id,
+            identity: employeeData.passport_no,
+            company_id: employeeData.company_id,
           },
-        },
-      }
-    )
+        }
+      )
 
-    const verifyEmployeeIdSignupResponseBody =
-      await verifyEmployeeIdSignupResponse.json()
-    // console.log('verifyEmployeeIdSignupResponseBody: ', verifyEmployeeIdSignupResponseBody)
+      const signupEmployeeIdResponseBody = await signupEmployeeIdResponse.json()
 
-    expect(verifyEmployeeIdSignupResponseBody.verification.token).not.toBeNull()
-    expect(
-      verifyEmployeeIdSignupResponseBody.verification.profile.first_name
-    ).toBe(employeeData.firstName)
-    expect(verifyEmployeeIdSignupResponseBody.verification.profile.phone).toBe(
-      employeeData.phone
-    )
+      expect(signupEmployeeIdResponseBody.is_signup).toBe(false)
+      expect(
+        typeof signupEmployeeIdResponseBody.verification_info.auth_challenge
+      ).toBe('string')
 
-    const firebaseToken = verifyEmployeeIdSignupResponseBody.verification.token
-
-    // 4. Signup Firebase
-    const firebaseAuthResponse = await request.post(endpoints.signupFirebase, {
-      data: {
-        token: firebaseToken,
-        returnSecureToken: true,
-      },
+      authChallenge =
+        signupEmployeeIdResponseBody.verification_info.auth_challenge
     })
 
-    const firebaseResponseBody = await firebaseAuthResponse.json()
-    // console.log('firebaseResponseBody Body: ', firebaseResponseBody)
+    await test.step('Step 2: Input Phone Number and Request OTP', async () => {
+      const verifyAddPhoneResponse = await request.post(
+        endpoints.verifySignupEmployeeIdAddPhone,
+        {
+          data: {
+            phone: employeeData.phone,
+            auth_challenge: authChallenge,
+          },
+        }
+      )
 
-    expect(firebaseResponseBody.kind).toBe(
-      'identitytoolkit#VerifyCustomTokenResponse'
-    )
-    expect(typeof firebaseResponseBody.idToken).toBe('string')
-    expect(typeof firebaseResponseBody.refreshToken).toBe('string')
-    expect(firebaseResponseBody.expiresIn).toBe('3600')
-    expect(firebaseResponseBody.isNewUser).toBe(false)
+      const verifyAddPhoneResponseBody = await verifyAddPhoneResponse.json()
 
-    const firebaseRefreshToken = firebaseResponseBody.refreshToken
+      expect(typeof verifyAddPhoneResponseBody.verification.ref_code).toBe(
+        'string'
+      )
 
-    // 5. Token Firebase Before PIN Code Verify
-    const secureTokenBeforePinResponse = await request.post(
-      endpoints.secureFirebaseToken,
-      {
+      refCode = verifyAddPhoneResponseBody.verification.ref_code
+    })
+
+    await test.step('Step 3: Verify OTP and Signup', async () => {
+      const verifyEmployeeIdSignupResponse = await request.post(
+        endpoints.verifySignupEmployeeIdAddPhoneVerify,
+        {
+          data: {
+            phone: employeeData.phone,
+            auth_challenge: authChallenge,
+            verification: {
+              ref_code: refCode,
+              code: employeeData.otp,
+            },
+          },
+        }
+      )
+
+      const verifyEmployeeIdSignupResponseBody =
+        await verifyEmployeeIdSignupResponse.json()
+
+      expect(
+        verifyEmployeeIdSignupResponseBody.verification.token
+      ).not.toBeNull()
+      expect(
+        verifyEmployeeIdSignupResponseBody.verification.profile.first_name
+      ).toBe(employeeData.firstName)
+      expect(
+        verifyEmployeeIdSignupResponseBody.verification.profile.phone
+      ).toBe(employeeData.phone)
+
+      firebaseToken = verifyEmployeeIdSignupResponseBody.verification.token
+    })
+
+    await test.step('Step 4: Signup Firebase with Custom Token', async () => {
+      const firebaseAuthResponse = await request.post(
+        endpoints.signupFirebase,
+        {
+          data: {
+            token: firebaseToken,
+            returnSecureToken: true,
+          },
+        }
+      )
+
+      const firebaseResponseBody = await firebaseAuthResponse.json()
+
+      expect(firebaseResponseBody.kind).toBe(
+        'identitytoolkit#VerifyCustomTokenResponse'
+      )
+      expect(typeof firebaseResponseBody.idToken).toBe('string')
+      expect(typeof firebaseResponseBody.refreshToken).toBe('string')
+      expect(firebaseResponseBody.expiresIn).toBe('3600')
+      expect(firebaseResponseBody.isNewUser).toBe(false)
+
+      firebaseRefreshToken = firebaseResponseBody.refreshToken
+    })
+
+    await test.step('Step 5: Get Secure Token from Firebase Before PIN Code Verify', async () => {
+      const secureTokenBeforePinResponse = await request.post(
+        endpoints.secureFirebaseToken,
+        {
+          data: {
+            grant_type: 'refresh_token',
+            refresh_token: firebaseRefreshToken,
+          },
+        }
+      )
+
+      const secureTokenBeforePinResponseBody =
+        await secureTokenBeforePinResponse.json()
+
+      expect(typeof secureTokenBeforePinResponseBody.access_token).toBe(
+        'string'
+      )
+      expect(secureTokenBeforePinResponseBody.expires_in).toBe('3600')
+      expect(secureTokenBeforePinResponseBody.token_type).toBe('Bearer')
+      expect(typeof secureTokenBeforePinResponseBody.refresh_token).toBe(
+        'string'
+      )
+      expect(typeof secureTokenBeforePinResponseBody.id_token).toBe('string')
+      expect(typeof secureTokenBeforePinResponseBody.user_id).toBe('string')
+      expect(typeof secureTokenBeforePinResponseBody.project_id).toBe('string')
+
+      idTokenBeforeVerifyPin = secureTokenBeforePinResponseBody.id_token
+    })
+
+    await test.step('Step 6: Verify PIN Code', async () => {
+      const verifyPinResponse = await request.post(endpoints.verifyPin, {
+        headers: {
+          Authorization: `Bearer ${idTokenBeforeVerifyPin}`,
+          'x-app-version': appVersion,
+        },
         data: {
-          grant_type: 'refresh_token',
-          refresh_token: firebaseRefreshToken,
+          pincode: employeeData.pinCode,
         },
-      }
-    )
+      })
 
-    const secureTokenBeforePinResponseBody =
-      await secureTokenBeforePinResponse.json()
-    // console.log(
-    //   'secureTokenBeforePinResponseBody Body: ',
-    //   secureTokenBeforePinResponseBody
-    // )
-
-    expect(typeof secureTokenBeforePinResponseBody.access_token).toBe('string')
-    expect(secureTokenBeforePinResponseBody.expires_in).toBe('3600')
-    expect(secureTokenBeforePinResponseBody.token_type).toBe('Bearer')
-    expect(typeof secureTokenBeforePinResponseBody.refresh_token).toBe('string')
-    expect(typeof secureTokenBeforePinResponseBody.id_token).toBe('string')
-    expect(typeof secureTokenBeforePinResponseBody.user_id).toBe('string')
-    expect(typeof secureTokenBeforePinResponseBody.project_id).toBe('string')
-
-    const idTokenBeforeVerifyPin = secureTokenBeforePinResponseBody.id_token
-
-    // 6. Verify PIN Code
-    const verifyPinResponse = await request.post(endpoints.verifyPin, {
-      headers: {
-        Authorization: `Bearer ${idTokenBeforeVerifyPin}`,
-        'x-app-version': '5.4.1',
-      },
-      data: {
-        pincode: employeeData.pinCode,
-      },
+      const verifyPinResponseBody = await verifyPinResponse.json()
+      expect(verifyPinResponseBody.message).toBe('Create PIN successfully')
     })
 
-    const verifyPinResponseBody = await verifyPinResponse.json()
-    // console.log('verifyPinResponseBody Body: ', verifyPinResponseBody)
+    await test.step('Step 7: Get Secure Token from Firebase After PIN Code Verify', async () => {
+      const secureTokenAfterPinResponse = await request.post(
+        endpoints.secureFirebaseToken,
+        {
+          data: {
+            grant_type: 'refresh_token',
+            refresh_token: firebaseRefreshToken,
+          },
+        }
+      )
 
-    expect(verifyPinResponseBody.message).toBe('Create PIN successfully')
+      const secureTokenAfterPinResponseBody =
+        await secureTokenAfterPinResponse.json()
 
-    // 7. Token Firebase After PIN Code Verify
-    const secureTokenAfterPinResponse = await request.post(
-      endpoints.secureFirebaseToken,
-      {
-        data: {
-          grant_type: 'refresh_token',
-          refresh_token: firebaseRefreshToken,
+      expect(typeof secureTokenAfterPinResponseBody.access_token).toBe('string')
+      expect(secureTokenAfterPinResponseBody.expires_in).toBe('3600')
+      expect(secureTokenAfterPinResponseBody.token_type).toBe('Bearer')
+      expect(typeof secureTokenAfterPinResponseBody.refresh_token).toBe(
+        'string'
+      )
+      expect(typeof secureTokenAfterPinResponseBody.id_token).toBe('string')
+      expect(typeof secureTokenAfterPinResponseBody.user_id).toBe('string')
+      expect(typeof secureTokenAfterPinResponseBody.project_id).toBe('string')
+
+      idTokenAfterVerifyPin = secureTokenAfterPinResponseBody.id_token
+    })
+
+    await test.step('Step 8: Get User Profile After Successfully Signup', async () => {
+      const userProfileResponse = await request.get(endpoints.userProfile, {
+        headers: {
+          Authorization: `Bearer ${idTokenAfterVerifyPin}`,
+          'x-app-version': appVersion,
         },
-      }
-    )
+      })
 
-    const secureTokenAfterPinResponseBody =
-      await secureTokenAfterPinResponse.json()
-    // console.log(
-    //   'secureTokenAfterPinResponseBody Body: ',
-    //   secureTokenAfterPinResponseBody
-    // )
+      const userProfileResponseBody = await userProfileResponse.json()
 
-    expect(typeof secureTokenAfterPinResponseBody.access_token).toBe('string')
-    expect(secureTokenAfterPinResponseBody.expires_in).toBe('3600')
-    expect(secureTokenAfterPinResponseBody.token_type).toBe('Bearer')
-    expect(typeof secureTokenAfterPinResponseBody.refresh_token).toBe('string')
-    expect(typeof secureTokenAfterPinResponseBody.id_token).toBe('string')
-    expect(typeof secureTokenAfterPinResponseBody.user_id).toBe('string')
-    expect(typeof secureTokenAfterPinResponseBody.project_id).toBe('string')
-
-    const idTokenAfterVerifyPin = secureTokenAfterPinResponseBody.id_token
-
-    // 8. Get User Profile After Successfully Signup
-    const userProfileResponse = await request.get(endpoints.userProfile, {
-      headers: {
-        Authorization: `Bearer ${idTokenAfterVerifyPin}`,
-        'x-app-version': appVersion,
-      },
+      expect(userProfileResponseBody.profile.signup_at).not.toBeNull()
+      expect(userProfileResponseBody.profile.first_name).toBe(
+        employeeData.firstName
+      )
+      expect(userProfileResponseBody.profile.phone).toBe(employeeData.phone)
+      expect(userProfileResponseBody.profile.employee_id).toBe(
+        employeeData.employee_id
+      )
+      expect(userProfileResponseBody.profile.company_id).toBe(
+        employeeData.company_id.toString()
+      )
+      expect(userProfileResponseBody.profile.passport_no).toBe(
+        employeeData.passport_no
+      )
+      expect(userProfileResponseBody.profile.phone).toBe(employeeData.phone)
     })
 
-    const userProfileResponseBody = await userProfileResponse.json()
-    // console.log('userProfileResponseBody Body: ', userProfileResponseBody)
+    await test.step('Step 9: Logout User', async () => {
+      const logoutResponse = await request.post(endpoints.userLogout, {
+        headers: {
+          Authorization: `Bearer ${idTokenAfterVerifyPin}`,
+          'x-app-version': appVersion,
+        },
+      })
 
-    expect(userProfileResponseBody.profile.signup_at).not.toBeNull()
-    expect(userProfileResponseBody.profile.first_name).toBe(
-      employeeData.firstName
-    )
-    expect(userProfileResponseBody.profile.phone).toBe(employeeData.phone)
-    expect(userProfileResponseBody.profile.employee_id).toBe(
-      employeeData.employee_id
-    )
-    expect(userProfileResponseBody.profile.company_id).toBe(
-      employeeData.company_id
-    )
-    expect(userProfileResponseBody.profile.passport_no).toBe(
-      employeeData.passport_no
-    )
-    expect(userProfileResponseBody.profile.phone).toBe(employeeData.phone)
-
-    // 9. Logout User
-    const logoutResponse = await request.post(endpoints.userLogout, {
-      headers: {
-        Authorization: `Bearer ${idTokenAfterVerifyPin}`,
-        'x-app-version': appVersion,
-      },
+      const logoutResponseBody = await logoutResponse.json()
+      expect(logoutResponseBody.message).toBe('Logout successfully')
     })
-
-    const logoutResponseBody = await logoutResponse.json()
-    // console.log('logoutResponseBody Body: ', logoutResponseBody)
-
-    expect(logoutResponseBody.message).toBe('Logout successfully')
   })
 })
